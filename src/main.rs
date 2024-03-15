d97209efd6bc2a928b97049015d67bfcc56e9d4a
dacmarcell <devmarcell@protonmail.com>
2024-03-15 11:33:34 -0300

    refactor: add types

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use rand::{Rng, thread_rng};
 
 fn main() {
     let password_length: u8 = 15;
-    let password = generate_password(password_length);
+    let password: String = generate_password(password_length);
     println!("Generated Password: {}", password);
 }
 
 fn generate_password(password_length: u8) -> String {
-    let mut result = String::new();
+    let mut result: String = String::new();
     for _ in 0..password_length {
         let number: u32 = thread_rng().gen_range(48..122);
         let characters: char = std::char::from_u32(number).unwrap();
